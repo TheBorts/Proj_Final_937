9ac6438840826954ec2e8e73a344cc3f556b5e67
The_Borts <bortolettolucas2005@gmail.com>
2025-06-23 14:20:05 -0300

    Arrumando shaders para suportar texturas

--- shaders/myshader.fs
+++ shaders/myshader.fs
@@ -1,4 +1,5 @@
 #version 330 core
+precision highp float;
 out vec4 FragColor;
 
 struct Material {
@@ -6,37 +7,46 @@ struct Material {
     vec3 diffuse;
     vec3 specular;    
     float shininess;
+    int hasTexture;
 }; 
 
 struct Light {
     vec3 position;
-
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
 };
 
 in vec3 FragPos;
-in vec3 Normal;  
-  
+in vec3 Normal;
+in vec2 TexCoord; 
+
+uniform sampler2D OurTexture;
 uniform vec3 viewPos;
 uniform Material material;
 uniform Light light;
 
 void main()
 {
-    vec3 ambient = light.ambient * material.ambient;
-  	
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * (diff * material.diffuse);
-    
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
+    
+    float diff = max(dot(norm, lightDir), 0.0);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
-        
+
+    vec3 ambient, diffuse;
+    if (material.hasTexture == 1) {
+        ambient = light.ambient * texture(OurTexture, TexCoord).rgb;
+        diffuse = light.diffuse * (diff * texture(OurTexture, TexCoord).rgb);
+    } else {
+        ambient = light.ambient * material.ambient;
+        diffuse = light.diffuse * (diff * material.diffuse);
+    }
+    
+    vec3 specular = light.specular * (spec * material.specular); 
     vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+
+    FragColor = vec4(result, 1.0);    
 }
